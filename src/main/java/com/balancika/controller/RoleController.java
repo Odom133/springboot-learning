51734b3c4f9c5d5fbbaf0109db35ee45119a3a64
Odom133 <barnodomnew@gmail.com>
2025-09-01 00:03:36 +0700

    Update RoleController.java

--- src/main/java/com/balancika/controller/RoleController.java
+++ src/main/java/com/balancika/controller/RoleController.java
@@ -1,13 +1,9 @@
 package com.balancika.controller;
 
-import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/role")
@@ -28,40 +24,68 @@ public class RoleController {
         data.add(payload);
         return ResponseEntity.ok(data);
     }
-    /*
-     * e.g: /role/:id
-     * payload: {
-     *   "name": "SuperAdmin"
-     * }
-     * */
-    // Edit role
-    @PatchMapping("/{id}")
-    ResponseEntity<Object> edit(@PathVariable("id") String id, @RequestBody Object payload){
-        return ResponseEntity.ok(id + payload);
+
+    // Get role
+    @GetMapping("/getAll")
+    ResponseEntity<List<Map<String, Object>>> getAll(){
+        return ResponseEntity.ok(data);
     }
-    /*
-     * e.g: /role/:id
-     * */
-    // Delete role by id
-    @DeleteMapping("/{id}")
-    ResponseEntity<Object> delete(@PathVariable("id") String id){
-        return ResponseEntity.ok(id);
-    }
-    /*
-     * e.g: /role/:id
-     * */
+
     // Get role by id
     @GetMapping("/{id}")
-    ResponseEntity<Object> get(@PathVariable("id") String id){
-        return ResponseEntity.ok(id);
+    ResponseEntity<List<Map<String, Object>>> getById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(data);
     }
-    /*
-     * e.g: /role
-     * */
-    // List all role
-    @GetMapping
-    ResponseEntity<Object> listAll(@RequestParam("listAll") String listAll){
-        return ResponseEntity.ok(listAll);
+
+
+    // edit role
+    @PatchMapping("/{id}")
+    ResponseEntity<List<Map<String, Object>>> update(@PathVariable("id") Long id, @RequestParam("name") String name){
+        return ResponseEntity.ok(data);
     }
+
+    // Delete
+    @DeleteMapping("/{id}")
+    ResponseEntity<List<Map<String, Object>>> deleteById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(data);
+    }
+
+
+
+//    /*
+//     * e.g: /role/:id
+//     * payload: {
+//     *   "name": "SuperAdmin"
+//     * }
+//     * */
+//    // Edit role
+//    @PatchMapping("/{id}")
+//    ResponseEntity<Object> edit(@PathVariable("id") String id, @RequestBody Object payload){
+//        return ResponseEntity.ok(id + payload);
+//    }
+//    /*
+//     * e.g: /role/:id
+//     * */
+//    // Delete role by id
+//    @DeleteMapping("/{id}")
+//    ResponseEntity<Object> delete(@PathVariable("id") String id){
+//        return ResponseEntity.ok(id);
+//    }
+//    /*
+//     * e.g: /role/:id
+//     * */
+//    // Get role by id
+//    @GetMapping("/{id}")
+//    ResponseEntity<Object> get(@PathVariable("id") String id){
+//        return ResponseEntity.ok(id);
+//    }
+//    /*
+//     * e.g: /role
+//     * */
+//    // List all role
+//    @GetMapping
+//    ResponseEntity<Object> listAll(@RequestParam("listAll") String listAll){
+//        return ResponseEntity.ok(listAll);
+//    }
 
 }
