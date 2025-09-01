8fdeb8760ae6a5a48c05cd38f4571d7d77f154f3
Odom133 <barnodomnew@gmail.com>
2025-09-01 21:40:19 +0700

    01-09-2025

--- src/main/java/com/balancika/controller/PriceCode.java
+++ src/main/java/com/balancika/controller/PriceCode.java
@@ -1,13 +1,64 @@
 package com.balancika.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/price_code")
 public class PriceCode {
+    private List<Map<String, Object>> data = new ArrayList<>();
 
+    //create
+    @PostMapping("/create")
+    ResponseEntity<List<Map<String, Object>>> create(@RequestBody Map<String, Object> payload){
+        data.add(payload);
+        return ResponseEntity.ok(data);
+    }
+    //List price code
+    @GetMapping("/list")
+    ResponseEntity<List<Map<String, Object>>> list(){
+        return ResponseEntity.ok(data);
+    }
+    // Search by Id
+    @GetMapping("/{id}")
+    ResponseEntity<Map<String, Object>> searchById(@PathVariable("id") Integer id){
+        Map<String, Object> result = null;
+        for (int i = 0; i < data.size(); i++) {
+                if (Integer.parseInt(data.get(i).get("id").toString()) == id){
+                    result = data.get(i);
+                }
+        }
+        return ResponseEntity.ok(result);
+    }
+    // edit price_code
+    @PatchMapping("/{id}")
+    ResponseEntity<Map<String, Object>> edit(@PathVariable("id") Integer id,@RequestParam("name") String name,@RequestParam("des")  String des){
+        Map<String, Object> result = null;
+        for (int i = 0 ; i < data.size(); i++){
+            if (Integer.parseInt((data.get(i).get("id").toString())) == id){
+                data.get(i).put("name", name);
+                data.get(i).put("des", des);
+                result = data.get(i);
+            }
+        }
+        return ResponseEntity.ok(result);
+    }
+
+    // delete price_code
+    @DeleteMapping("/{id}")
+    ResponseEntity<String> delete(@PathVariable("id") Integer id){
+        for (int i =0 ; i < data.size(); i++){
+            if (Integer.parseInt(data.get(i).get("id").toString()) == id){
+                data.remove(i);
+            }
+        }
+        return ResponseEntity.status(201).body("Delete successfully!");
+    }
 
 
 }
--- src/main/java/com/balancika/controller/UserController.java
+++ src/main/java/com/balancika/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.balancika.controller;
 
 
-import jakarta.websocket.OnClose;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -57,36 +56,5 @@ public class UserController {
         }
         return ResponseEntity.noContent().build();
     }
-
-    /*
-    // Search user
-    // /user/search?query=...
-    // /user?query=...
-    @GetMapping
-    ResponseEntity<Object> search(@RequestParam("query") String query) {
-        return ResponseEntity.ok(query);
-    }
-
-    // Get User by ID
-    @GetMapping("/{id}")
-    ResponseEntity<Object> get(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(id);
-    }
-    // Create User
-    @PostMapping
-    ResponseEntity<Object> create(@RequestBody Object payload) {
-        return ResponseEntity.ok(payload);
-    }
-    // Update User
-    // /user/1?name=...&email=...
-    @PatchMapping("/{id}")
-    ResponseEntity<Object> updatePartially(@PathVariable("id") Long id, @RequestParam("name") String name, @RequestParam("email") String email) {
-        return ResponseEntity.ok(id);
-    }
-
-    // Delete User by ID
-    // List all users
-
-     */
 
 }
